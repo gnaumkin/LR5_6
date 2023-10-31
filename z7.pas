087a02e5e78a9e157a4654c181996cd1fbb49996
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-10-31 12:13:16 +0300

    Update z7.pas

--- z7.pas
+++ z7.pas
@@ -3,7 +3,7 @@ const
   N = 10; // Размер массива
 var
   Arr: array[1..N] of integer;
-  i, currentLength, maxLength: integer;
+  i, cl, maxl: integer;
 begin
   // Заполнение массива (для примера, вы можете ввести свои значения)
   writeln('Введите элементы массива:');
@@ -12,20 +12,20 @@ begin
     write('Элемент ', i, ': ');
     readln(Arr[i]);
   end;
-  currentLength := 1; // Текущая длина неубывающего участка
-  maxLength := 1;   // Максимальная длина неубывающего участка
+  cl := 1; // Текущая длина неубывающего участка
+  maxl := 1;   // Максимальная длина неубывающего участка
   for i := 2 to N do
   begin
     if Arr[i] >= Arr[i - 1] then
-      currentLength := currentLength + 1
+      cl := cl + 1
     else
     begin
-      if currentLength > maxLength then
-        maxLength := currentLength;
-      currentLength := 1;
+      if cl > maxl then
+        maxl := cl;
+      cl := 1;
     end;
   end;
-  if currentLength > maxLength then
-    maxLength := currentLength;
-  writeln('Максимальная длина неубывающего участка: ', maxLength);
+  if cl > maxl then
+    maxl := cl;
+  writeln('Максимальная длина неубывающего участка: ', maxl);
 end.

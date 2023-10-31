db677eeb6c2f5fdfb1f6e57ff8891901437c156b
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-10-31 13:34:09 +0300

    Create z13.pas

---
+++ z13.pas
@@ -0,0 +1,36 @@
+program SwapMinMax;
+const
+  N = 20;
+var
+  numbers: array[1..N] of Integer;
+  i, minIndex, maxIndex, temp: Integer;
+begin
+  // Заполняем массив случайными числами
+  randomize;
+  for i := 1 to N do
+    numbers[i] := random(100) - 50; // генерируем числа в диапазоне -50..50
+  // Выводим исходный массив
+  writeln('Исходный массив:');
+  for i := 1 to N do
+    write(numbers[i], ' ');
+  writeln;
+  // Находим индексы наибольшего и наименьшего элементов
+  minIndex := 1;
+  maxIndex := 1;
+  for i := 2 to N do
+  begin
+    if numbers[i] < numbers[minIndex] then
+      minIndex := i;
+    if numbers[i] > numbers[maxIndex] then
+      maxIndex := i;
+  end;
+  // Меняем местами наибольший и наименьший элементы
+  temp := numbers[minIndex];
+  numbers[minIndex] := numbers[maxIndex];
+  numbers[maxIndex] := temp;
+  // Выводим измененный массив
+  writeln('Массив после замены наибольшего и наименьшего элементов:');
+  for i := 1 to N do
+    write(numbers[i], ' ');
+  writeln;
+end.
